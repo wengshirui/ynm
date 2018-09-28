1f70363abff02058e7c327c8296a839dffbf6285
wengshirui <wengrui2046@sina.com>
2018-09-28 16:26:24 +0800

    #公司信息增加上传营业执照的图片

--- apps/company/models.py
+++ apps/company/models.py
@@ -8,6 +8,7 @@ class ComInfoModel(models.Model):
     ''' 公司的基本信息 '''
     name = models.CharField(verbose_name='公司名称',max_length=100,default='')
     trade = models.ForeignKey(TradeModel,verbose_name='所属行业',default='',blank=True,on_delete=models.CASCADE)
+    img = models.ImageField(verbose_name='营业执照', max_length=200, null=True, blank=True, upload_to='com_img/%Y/%m')
     address = models.CharField(verbose_name='公司地址',max_length=100,null=True,blank=True,default='')
     credit_no = models.CharField(verbose_name='信用代码',max_length=100,default='',blank=True,)
     desc = models.TextField(verbose_name='公司简介',default='')
